2e85d3dae3b60c2cb69534ddd2d21b8c2910a4f3
Pglos <gabriel.leiteost@gmail.com>
2025-11-25 08:53:15 -0300

    codigo do desafio

--- DesafioNivelHeroi/desafioheroi.js
+++ DesafioNivelHeroi/desafioheroi.js
@@ -1,1 +1,27 @@
-console.log("ola munod")
+// Variáveis
+const nomeDoHeroi = "Pedro";
+let xpDoHeroi = 8001;
+let nivel;
+
+// Estrutura de decisão correta
+if (xpDoHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpDoHeroi >= 5001 && xpDoHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+    nivel = "Imortal";
+} else if (xpDoHeroi >= 10001) {
+    nivel = "Radiante";
+} else {
+    nivel = "Nível desconhecido";
+}
+
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivel}`);
