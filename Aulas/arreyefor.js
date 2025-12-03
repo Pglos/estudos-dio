e8ca522f44439aec8fd017344a14500029ef9914
Pglos <gabriel.leiteost@gmail.com>
2025-12-03 09:42:45 -0300

    exercicofor

--- Aulas/arreyefor.js
+++ Aulas/arreyefor.js
@@ -1,4 +1,4 @@
-const notas = [];
+/* const notas = [];
 
 notas.push(5);
 notas.push(8);
@@ -14,3 +14,22 @@ for (let i = 0 ; i < notas.length; i++){
 
 const media = soma / notas.length;
 console.log(media);
+*/
+
+/*
+const num = 5;
+for (let index = 1; index <= 10 ; index++) {
+    console.log(index * num)
+    
+}*/
+
+const num = [1,2,3,4,5,6,7,8,9];
+
+for (let index = 0; index < num.length; index++) {
+    const numero = num[index];
+
+    if (numero % 2 === 0){
+        console.log(numero)
+    }
+    
+}
