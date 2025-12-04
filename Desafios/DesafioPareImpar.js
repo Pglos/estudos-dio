da0da9aaa52d552db5a5dfac4cdc5595cdc314b4
Pglos <gabriel.leiteost@gmail.com>
2025-12-04 09:24:09 -0300

    desafiopar

--- Desafios/DesafioPareImpar.js
+++ Desafios/DesafioPareImpar.js
@@ -1,3 +1,27 @@
 const {gets , print} = require('../Aulas/gets/funcoegets');
 
 
+const n = gets();
+let maiorNumeropar = null;
+let menorNumeroImpar = null;
+
+for (let index = 0; index < n ; index++) {
+    
+    const numero = gets();
+    
+    if(numero % 2 === 0 ){
+
+        if(maiorNumeropar === null || numero > maiorNumeropar){
+            maiorNumeropar = numero;
+        }
+    }else {
+
+        if(  menorNumeroImpar === null || numero < menorNumeroImpar){
+            menorNumeroImpar = numero;
+    }
+    
+}
+}
+
+print(maiorNumeropar);
+print(menorNumeroImpar);
